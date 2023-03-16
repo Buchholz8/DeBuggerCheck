5e4b7ab5181bb2f8918e8c84fc696b6798fae707
markb <markbuchholz8@gmail.com>
2023-03-16 17:35:55 -0600

    fixes

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 let username="mark";
 let user_age=18;
 let is_member=true;
-if(username === "admin" === user_age === 18 === is_member === true){
+if(username === "admin" || (user_age > 30 && is_member === true)){
     console.log("Welcome Mark")
 } else {
     console.log ("welcome")
