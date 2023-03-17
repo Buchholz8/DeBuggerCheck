6d5a88896cb828d758d849aa22c7c3b0dee50695
markb <markbuchholz8@gmail.com>
2023-03-16 18:57:21 -0600

    numbers

--- js/app.js
+++ js/app.js
@@ -8,3 +8,7 @@ if(username === "admin" || (user_age >= 30 && is_member === true)){
 }
 console.log(`Greetings ${username}`);
 console.log(`Tou are: ${user_age}`);
+let max_volume = 20.5
+let current_volume =1/8
+console.log(max_volume / current_volume);
+console.log(max_volume / current_volume * 100);
